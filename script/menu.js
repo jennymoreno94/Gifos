55c12580d05a8d5ec6381823ebe16046d809ecd1
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-05-05 01:09:15 -0500

    implementación header y inicio busqueda

--- script/menu.js
+++ script/menu.js
@@ -1,19 +1,70 @@
+/*HEADER*/
 
-const theme = document.querySelector('#mode')
-
-
-theme.addEventListener("click",()=> {
+/*const theme = document.querySelector('#mode')
+theme.addEventListener("click", () => {
     const dark = document.documentElement.getAttribute('data-theme')
-    if(dark === null) {
-        document.documentElement.setAttribute('data-theme','dark')
-        localStorage.setItem('data-theme','dark')
+    if (dark === null) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+        localStorage.setItem('data-theme', 'dark')
     } else {
         document.documentElement.removeAttribute('data-theme')
-        localStorage.removeItem('data-theme')  
+        localStorage.removeItem('data-theme')
     }
- 
+
 })
+
 function Menu() {
     var element = document.body;
     element.classList.toggle("visible_menu");
+}*/
+/*HEADER*/
+
+/*BUSQUEDA*/
+const data = [{
+        name: "joy",
+        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…WU3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kmcT1qbw"
+    },
+    {
+        name: "joy joy",
+        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…dmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95JnE9am8"
+    },
+    {
+        name: "joy joy joy",
+        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95K2pveSZxPWpv"
+    },
+    {
+        name: "joyeux",
+        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…TdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3lldXgmcT1qbw"
+    },
+    {
+        name: "joker",
+        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb2tlciZxPWpv"
+    }
+]
+
+let search = document.getElementById("search");
+search.addEventListener("input", (e) => {
+
+    let FilterData = [];
+    debugger;
+    if (e.target.value) {
+        if (data.length > 0) {
+            FilterData = FilterData.filter((data) => {
+                debugger;
+                data.name.includes(e.target.name)
+            });
+            FilterData.map((match) => `<li>${match.name}</li>`)
+        }
+    }
+
+    console.log(FilterData)
+    showSearchArray(FilterData)
+})
+
+function showSearchArray(data) {
+    const html = data
+    document.querySelector('ul').innerHTML = html
 }
+
+
+/*BUSQUEDAs*/
