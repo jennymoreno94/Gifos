90b89ae6554b6f3bd59eaa9c0bdc30c72dc7e0e1
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-05-05 22:24:59 -0500

    se realiza ajuste input buscar

--- script/menu.js
+++ script/menu.js
@@ -21,45 +21,65 @@ function Menu() {
 
 /*BUSQUEDA*/
 const data = [{
-        name: "joy",
-        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…WU3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kmcT1qbw"
-    },
-    {
-        name: "joy joy",
-        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…dmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95JnE9am8"
-    },
-    {
-        name: "joy joy joy",
-        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95K2pveSZxPWpv"
-    },
-    {
-        name: "joyeux",
-        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…TdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3lldXgmcT1qbw"
-    },
-    {
-        name: "joker",
-        analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb2tlciZxPWpv"
-    }
+    name: "joy",
+    analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…WU3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kmcT1qbw"
+},
+{
+    name: "joy joy",
+    analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…dmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95JnE9am8"
+},
+{
+    name: "joy joy joy",
+    analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3kram95K2pveSZxPWpv"
+},
+{
+    name: "joyeux",
+    analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…TdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb3lldXgmcT1qbw"
+},
+{
+    name: "joker",
+    analytics_response_payload: "e=ZXZlbnRfdHlwZT1UQUdfU0VBUkNIJmNpZD1mNjYzY2VhNTRk…3ZTdmMWU1M2E2MzM2YjkzZTRjZTgmbmFtZT1qb2tlciZxPWpv"
+}
 ]
 
 let search = document.getElementById("search");
 search.addEventListener("input", (e) => {
-            const suggestionsList = document.getElementById(`suggestions-list`);
-            if (e.target.value) {
-                let FilterData = [];
-                if (data.length > 0) {
-                    FilterData = data.filter((da) => da.name.includes(e.target.value));
-                    const searchAutocomplete =
-                        `<ul>
-                            ${FilterData.map(item => `
-                            <li class="option-list"><i class="fa fa-search"></i>${item.name}</li>
-                            `).join('')}
-                        </ul>`;  
-                suggestionsList.innerHTML = FilterData.length !== 0 ? searchAutocomplete : ''           
-            }     
+    const suggestionsList = document.getElementById(`suggestions-list`);
+    if (e.target.value) {
+        let FilterData = [];
+        if (data.length > 0) {
+            FilterData = data.filter((da) => da.name.includes(e.target.value));
+            const searchAutocomplete =
+                `<ul>
+                    ${FilterData.map(item => `
+                    <li class="list"><i class="fa fa-search"></i>${item.name}</li>
+                    `).join('')}
+                </ul>`;
+
+            if (FilterData.length !== 0) {
+                suggestionsList.innerHTML = searchAutocomplete
+                search.style.borderBottomRightRadius = '0em'
+                search.style.borderBottomLeftRadius = '0em'
             } else {
                 suggestionsList.innerHTML = ''
+                search.style.borderBottomRightRadius = '2em'
+                search.style.borderBottomLeftRadius = '2em'
             }
+        }
+    } else {
+        search.style.borderBottomRightRadius = '2em'
+        search.style.borderBottomLeftRadius = '2em'
+        suggestionsList.innerHTML = ''
+    }
+
+    const optionList = document.querySelectorAll(".list");
+    optionList.forEach(li => li.addEventListener("click", event => {
+        suggestionsList.innerHTML = ''
+        search.style.borderBottomRightRadius = '2em'
+        search.style.borderBottomLeftRadius = '2em'
+        search.value = event.currentTarget.textContent
+    }))
+
 })
 
 /*BUSQUEDAs*/
