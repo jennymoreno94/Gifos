4bfe986bd55ac8dd5901b93b01cd4819c53cb690
Jenny Moreno Vallejo <jenny.moreno@infotrack.com.co>
2021-05-16 00:01:34 -0500

    cambio texto modo nocturno y diurno

--- script/menu.js
+++ script/menu.js
@@ -1,12 +1,15 @@
 /*HEADER*/
 
 const theme = document.querySelector('#mode')
+let mode = document.getElementById("mode");
 theme.addEventListener("click", () => {
     const dark = document.documentElement.getAttribute('data-theme')
     if (dark === null) {
+        mode.innerText = "Modo Diurno"
         document.documentElement.setAttribute('data-theme', 'dark')
         localStorage.setItem('data-theme', 'dark')
     } else {
+        mode.innerText = "Modo Nocturno"
         document.documentElement.removeAttribute('data-theme')
         localStorage.removeItem('data-theme')
     }
